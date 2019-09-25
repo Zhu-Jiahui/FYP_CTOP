fbae77366e7bd3b6fda3e14d09e2d67b67f2c3f4
Zhu Jiahui <jiahuizhu1@gmail.com>
2019-09-25 23:04:42 +0800

    test

--- main.cpp
+++ main.cpp
@@ -430,7 +430,7 @@ int main() {
     }
     simulatedAnnealing(CUSTOMERS, &unassignCustomersList, &assignedCustomersList, vehicles);
     
-    cout << "testing github 456 " << '\n';
+    cout << "testing github 789 " << '\n';
     
     return 0;
 }
